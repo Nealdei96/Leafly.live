fbb1c1b8c82459a44881c7c40fc2d5c48b3d76e4
Nealdei96 <nealdeiandre96@gmail.com>
2026-05-22 21:05:16 -0400

    Vite 5 configuration with React plugin

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    strictPort: false,
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  preview: {
+    port: 4173,
+  },
+})
